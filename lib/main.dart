b24603bbb41998a2b4b393fd9d5c6b7dc8aecc61
Uzukiis <kabuciakk@gmail.com>
2026-02-17 14:46:25 +0100

    basic layout

--- lib/main.dart
+++ lib/main.dart
@@ -9,10 +9,19 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return  MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        body: Column(
+          
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const SizedBox(width: double.infinity),
+            const Spacer(flex: 1,),
+            Text('00:00:00', style: TextStyle(fontSize: 56)),
+            const Spacer(flex: 1,),
+            ElevatedButton(onPressed: (){}, style: ElevatedButton.styleFrom(minimumSize: Size(110, 55)), child: Icon(Icons.play_arrow, size: 32,),),
+            const SizedBox(height: 75,),
+          ],
         ),
       ),
     );
